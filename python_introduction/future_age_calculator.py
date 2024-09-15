b6b4f76ebbf16e0d3e9a91a3a2f6cf73b20c2c0f
Betel Dessalegn <beteldessalegn0147@gmail.com>
2024-09-15 17:09:59 +0300

    age calculator q4

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,11 @@
+current_year = 2023
+
+future_year = 2050 
+
+user_age = int(input("How old are you? "))
+
+years_to_add = future_year - current_year
+
+future_age = user_age + years_to_add
+
+print(f"In {future_year}, you will be {future_age} years old.")
