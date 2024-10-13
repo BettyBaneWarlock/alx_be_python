a08d6fbac732e303f66587112c18a90e0170a620
Betel Dessalegn <beteldessalegn0147@gmail.com>
2024-10-13 11:29:21 +0300

    w7 q2 3

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -12,7 +12,7 @@ class Rectangle(Shape):
         self.width=width
     
     def area(self):
-        return self.length*self.width
+        return self.length * self.width
 
 # Derived class for Circle
 class Circle(Shape):
